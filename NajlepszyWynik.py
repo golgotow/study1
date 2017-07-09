cdce05ceb2ae229d665d4d997d5f352e751940f9
golgotow <sobotaprzemyslaw@gmail.com>
2017-07-09 12:37:12 +0200

    08.07.2017

---
+++ NajlepszyWynik.py
@@ -0,0 +1,46 @@
+# program najlepszy wynik
+
+
+scores = []
+
+choice = None
+
+while choice != "0":
+
+    print(
+        """
+        Najlepsze Wyniki
+        
+        0 - zakończ
+        1 - pokaż wyniki
+        2 - dodaj wynik
+        3 - usuń wynik
+        4 - posortuj wyniki
+        """
+    )
+choice = input("Wybierasz:")
+print()
+
+#Zakończ program
+if choice == "0":
+    print("Do widzenia!")
+
+#Wypisz tabelę najlepszych wyników
+
+elif choice == "1":
+    print("Najlepszego wyniki")
+    for score in scores:
+        print(score)
+#Dodaj wynik
+elif choice == "2":
+    score = int(input("Jaki wynik uzyskałeś?: "))
+    scores.append(score)
+#usuń wynik
+elif choice == "3":
+    score = int(input("Który wynik usunąć ? "))
+    if score in scores:
+        scores.remove(score)
+    else:
+        print("nie ma na liście wyników")
+
+
