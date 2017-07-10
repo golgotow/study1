5725e9e987588b6cb96bc87d47928f63fc61e3db
golgotow <sobotaprzemyslaw@gmail.com>
2017-07-10 13:05:48 +0200

    10.7.2017

--- NajlepszyWynik.py
+++ NajlepszyWynik.py
@@ -18,29 +18,34 @@ while choice != "0":
         4 - posortuj wyniki
         """
     )
-choice = input("Wybierasz:")
-print()
-
-#Zakończ program
-if choice == "0":
-    print("Do widzenia!")
-
-#Wypisz tabelę najlepszych wyników
-
-elif choice == "1":
-    print("Najlepszego wyniki")
-    for score in scores:
-        print(score)
+    choice = input("Wybierasz: ")
+    print()
+    #Zakończ program
+    if choice == "0":
+	    print("Do widzenia!")
+    #Wypisz tabelę najlepszych wyników
+    elif choice == "1":
+        print("Najlepszego wyniki")
+        for score in scores:
+        	print(score)
 #Dodaj wynik
-elif choice == "2":
-    score = int(input("Jaki wynik uzyskałeś?: "))
-    scores.append(score)
+    elif choice == "2":
+        score = int(input("Jaki wynik uzyskałeś?: "))
+        scores.append(score)
 #usuń wynik
-elif choice == "3":
-    score = int(input("Który wynik usunąć ? "))
-    if score in scores:
-        scores.remove(score)
+    elif choice == "3":
+        score = int(input("Który wynik usunąć ? "))
+        if score in scores:
+            scores.remove(score)
+        else:
+            print("nie ma na liście wyników")
+# posortuj wyniki
+
+    elif choice == "4":
+        scores.sort(reverse=True)
+        print(scores)
+# nieznana opcja
     else:
-        print("nie ma na liście wyników")
+        print("opcja nie jest znana.")
 
 
