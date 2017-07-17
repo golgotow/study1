07d96d43c521dcbebe0209e7f6c7e786c6ed82ed
golgotow <sobotaprzemyslaw@gmail.com>
2017-07-17 12:53:02 +0200

    17.7.2017

--- os_module.py
+++ os_module.py
@@ -1,14 +1,20 @@
 import os
 
 gohome = open("/tmp/testprzemek.txt", 'w')
-gohome.write("#!/bin/bash\n")
-gohome.write("df -h")
-gohome.write("czesc\n"
-             "jeden\n"
-             "dwa\n"
-             "trzy\n"
-             "cztery\n"
-             "piec\n")
+#gohome.write("#!/usr/bin/env python3\n")
+#gohome.write('print("nosz kurwa")\n')
+#gohome.write("a = 5\n")
+#gohome.write("if a == 5:\n")
+#gohome.write("  print ('a jest 5')\n")
+#gohome.write("else:\n")
+#gohome.write("  print ('a nie jest 5')\n")
+gohome.write("#!/usr/bin/env python3\n"
+             'print("nosz kurwa")\n'
+             "a = 5\n"
+             "if a == 5:\n"
+             "  print ('a jest 5')\n"
+             "else:\n"
+             "  print ('a nie jest 5')\n")
 gohome.close()
 #os.system("chmod +x /tmp/testprzemek.txt")
 #os.system("/tmp/testprzemek.txt")
