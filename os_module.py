057a0411c92790156ab02c45cfe3368f2b93d2f3
golgotow <sobotaprzemyslaw@gmail.com>
2017-07-17 09:27:52 +0200

    17.7.2017

--- os_module.py
+++ os_module.py
@@ -1,4 +1,14 @@
 import os
 
 gohome = open("/tmp/testprzemek.txt", 'w')
-gohome.write("czesc\n")
+gohome.write("#!/bin/bash\n")
+gohome.write("df -h")
+gohome.write("czesc\n"
+             "jeden\n"
+             "dwa\n"
+             "trzy\n"
+             "cztery\n"
+             "piec\n")
+gohome.close()
+#os.system("chmod +x /tmp/testprzemek.txt")
+#os.system("/tmp/testprzemek.txt")
